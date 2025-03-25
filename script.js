408359e61de2491d7c3cb33c887562d6ab394dd8
olawale3334 <olawale3334@gmail.com>
2025-03-25 22:00:42 +0100

    new added

---
+++ script.js
@@ -0,0 +1,60 @@
+let age =5
+age+=1
+console.log(age)
+
+let a =5
+let b =10
+a +=b
+let result= a+b
+console.log(result)
+
+let c =5
+let d =2
+c-=1
+let r = c-d
+console.log(r)
+
+let e =2
+let f = 10
+e *= f
+console.log(e)
+let outcome = e*f
+console.log(outcome)
+
+let g = 3
+let h=5
+g /=h
+let outc = g /h
+console.log(outc)
+
+let i = 40
+let j =5
+i **=j
+let outco =i+j
+console.log(outco)
+
+let k =2
+let l=5
+console.log((k++)*(l--))
+console.log((k)*(l))
+
+//comparison
+
+let m = 5
+let n =4
+let o =9
+let p =5
+let q = "4"
+console.log(m==p)
+console.log(n==q)
+console.log(m===p)
+console.log(n===q)
+console.log(m!==p)
+console.log(n!==q)
+console.log(o==p)
+console.log(o!==p)
+
+
+
+
+

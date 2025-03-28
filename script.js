e76a702ab66d96aa93de9c3d3a9a050d433588fd
olawale3334 <olawale3334@gmail.com>
2025-03-28 09:39:48 +0100

    new added

--- script.js
+++ script.js
@@ -1,60 +1,112 @@
-let age =5
-age+=1
-console.log(age)
+// let age =5
+// age+=1
+// console.log(age)
 
-let a =5
-let b =10
-a +=b
-let result= a+b
-console.log(result)
+// let a =5
+// let b =10
+// a +=b
+// let result= a+b
+// console.log(result)
 
-let c =5
-let d =2
-c-=1
-let r = c-d
-console.log(r)
+// let c =5
+// let d =2
+// c-=1
+// let r = c-d
+// console.log(r)
 
-let e =2
-let f = 10
-e *= f
-console.log(e)
-let outcome = e*f
-console.log(outcome)
+// let e =2
+// let f = 10
+// e *= f
+// console.log(e)
+// let outcome = e*f
+// console.log(outcome)
 
-let g = 3
-let h=5
-g /=h
-let outc = g /h
-console.log(outc)
+// let g = 3
+// let h=5
+// g /=h
+// let outc = g /h
+// console.log(outc)
 
-let i = 40
-let j =5
-i **=j
-let outco =i+j
-console.log(outco)
+// let i = 40
+// let j =5
+// i **=j
+// let outco =i+j
+// console.log(outco)
 
-let k =2
-let l=5
-console.log((k++)*(l--))
-console.log((k)*(l))
+// let k =2
+// let l=5
+// console.log((k++)*(l--))
+// console.log((k)*(l))
 
-//comparison
+// //comparison
 
-let m = 5
-let n =4
-let o =9
-let p =5
-let q = "4"
-console.log(m==p)
-console.log(n==q)
-console.log(m===p)
-console.log(n===q)
-console.log(m!==p)
-console.log(n!==q)
-console.log(o==p)
-console.log(o!==p)
+// let m = 5
+// let n =4
+// let o =9
+// let p =5
+// let q = "4"
+// console.log(m==p)
+// console.log(n==q)
+// console.log(m===p)
+// console.log(n===q)
+// console.log(m!==p)
+// console.log(n!==q)
+// console.log(o==p)
+// console.log(o!==p)
+
+ // class asssignment
+//   let a =10
+//   let b = 5
+//   let c = 10
+//   let d = 15
+//   let e =11
+//   console.log(a >b)
+//   console.log(!a > b)
+//   console.log((a>b)&&(a>c))
+//   console.log((a+b > d) || (a+b ==d) )
+//   console.log( (c+=5)==d  || (a++ )==e )
 
 
 
+//   let a 
+//   let b = 2
+//   let c = 10
+//   let d = '5'
+//   let e =0
+//   console.log(5|3)
 
+ // assignment
+// let a =5
+// let b = 10
+// let c = 20
+// let d = 13
+// let e = 5
+// let f = 13
+// let g =  14
+// let  result = a==b
+// console.log(result)
+// console.log(a*b == d)
+// let ans = a%b == c%b
+// console.log(ans)
+// console.log(d>f)
+//  let know = (a*b) +c == (g*5)
+//  console.log(know)
+//  console.log(5==5)
+//  console.log( (2+3)*(4-1))
+//  let number = 25
+//  let squareRoot = Math.sqrt(number)
+//  console.log( squareRoot)
 
+ 
+// string operation
+// +
+//   let me =" kc"
+//   let school ='dropout'
+//   let greeting = me +'goodmorning,'
+//   let tlgreeting =`good morning, ${me} ${school}`
+//  console.log(greeting)
+//  console.log(tlgreeting)
+
+ // < & >
+
+ 
